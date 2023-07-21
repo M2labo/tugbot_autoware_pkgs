1ef8b0496f17373ae7bc3a22846d9fdd8e90ebe4
hiroki_mori3 <porizou.electro@gmail.com>
2023-07-21 15:49:01 +0900

    update

--- launch/tugbot_autoware.launch.py
+++ launch/tugbot_autoware.launch.py
@@ -8,4 +8,9 @@ def generate_launch_description():
             executable='ackermann_to_twist_node',
             name='ackermann_to_twist',
         ),
+        Node(
+            package='tugbot_autoware_pkg',
+            executable='steering_report_publisher_node',
+            name='steering_report_publisher',
+        ),
     ])
--- src/steering_report_publisher.cpp
+++ src/steering_report_publisher.cpp
@@ -1,8 +1,5 @@
 #include "rclcpp/rclcpp.hpp"
-#include "nav_msgs/msg/odometry.hpp"
 #include "autoware_auto_vehicle_msgs/msg/steering_report.hpp"
-#include "tf2/LinearMath/Quaternion.h"
-#include "tf2/LinearMath/Matrix3x3.h"
 
 class SteeringAnglePublisher : public rclcpp::Node
 {
@@ -11,32 +8,21 @@ public:
   : Node("steering_angle_publisher")
   {
     publisher_ = this->create_publisher<autoware_auto_vehicle_msgs::msg::SteeringReport>("/vehicle/status/steering_status", 10);
-    subscription_ = this->create_subscription<nav_msgs::msg::Odometry>(
-      "/localization/kinematic_state", 10, std::bind(&SteeringAnglePublisher::odom_callback, this, std::placeholders::_1));
+    timer_ = this->create_wall_timer(std::chrono::milliseconds(1000/30), std::bind(&SteeringAnglePublisher::publish_steering_status, this));
   }
 
 private:
-  void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
+  void publish_steering_status()
   {
-    tf2::Quaternion q(
-      msg->pose.pose.orientation.x,
-      msg->pose.pose.orientation.y,
-      msg->pose.pose.orientation.z,
-      msg->pose.pose.orientation.w);
-
-    tf2::Matrix3x3 m(q);
-    double roll, pitch, yaw;
-    m.getRPY(roll, pitch, yaw);
-
     autoware_auto_vehicle_msgs::msg::SteeringReport steering_msg;
     steering_msg.stamp = this->now();
-    steering_msg.steering_tire_angle = static_cast<float>(yaw);
+    steering_msg.steering_tire_angle = 0.0f;
 
     publisher_->publish(steering_msg);
   }
 
   rclcpp::Publisher<autoware_auto_vehicle_msgs::msg::SteeringReport>::SharedPtr publisher_;
-  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr subscription_;
+  rclcpp::TimerBase::SharedPtr timer_;
 };
 
 int main(int argc, char * argv[])
